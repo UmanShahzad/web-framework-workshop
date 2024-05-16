c7213bab354f94bcbda7ddddbe66d77d932d1222
Uman Shahzad <uman@mslm.io>
2024-05-16 09:59:52 +0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+const n = (tag, props, children) => ({
+    tag,
+    props,
+    children,
+});
+
+const createElement = (vnode) => {
+    // TODO
+}
+
+const patch = (ele, oldVnode, newVnode) => {
+    // TODO
+}
+
+const root = n('div', { id: 'app' }, ['Hello World!']);
+
+document.body.innerHTML = createElement();
